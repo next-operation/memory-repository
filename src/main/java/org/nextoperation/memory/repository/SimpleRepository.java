f40154e31d16d1a52271aae47aaf8c69d69abba3
haril <songkg7@gmail.com>
2022-09-08 02:14:26 +0900

    refactor: class naming

--- src/main/java/org/nextoperation/memory/repository/SimpleRepository.java
+++ src/main/java/org/nextoperation/memory/repository/SimpleRepository.java
@@ -5,7 +5,12 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class IndexKeyRepository extends MemoryRepository<String, Long> {
+/**
+ * A repository with index as key.
+ *
+ * @author haril song
+ */
+public class SimpleRepository extends MemoryRepository<String, Long> {
 
     @Override
     protected Map<Long, String> groupBy(List<? extends String> elements) {
